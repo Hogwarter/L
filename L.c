8b0640af1bae20b5a75aaaf77cf41715b5e48797
Hogwarter <37118117+Hogwarter@users.noreply.github.com>
2022-10-04 21:35:14 +0300

    Add files via upload

---
+++ L.c
@@ -0,0 +1,19 @@
+#define Dear int main(){ 
+#define Luxi char* e="I love you!\n" 
+#define I so="much"; 
+#define love typedef struct{char* you_}i; 
+#define you i love_ = {you_: so};for(int i=(int)love_ 
+#define _You_ you_,j=0; 
+#define are j<1; 
+#define everything j++){printf( 
+#define to "%s", e 
+#define me );} 
+#define Sincerely int vz 
+#define The wx=0; 
+#define GF return wx;} 
+
+Dear Luxi, 
+    I love you. _You_ are everything to me 
+    		Sincerely,  
+    			The GF 
+/*bloody snuggle bug*/
